f11ef28ae236ea299b0e538e79f3ed28a67c3a96
Rakesh Kumar <rakeshkrdss19@gmail.com>
2026-07-21 17:46:35 +0530

    Enable CORS for deployed frontend

--- src/main/java/com/skylark/business_agent/controller/AIController.java
+++ src/main/java/com/skylark/business_agent/controller/AIController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class AIController {
 
     private final BIService biService;
